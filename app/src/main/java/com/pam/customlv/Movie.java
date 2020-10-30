05db70c11355c78a356015c3776a96c96d82729a
wahyuspb <wahyusuryo153@gmail.com>
2020-10-30 23:47:29 +0700

    upload

---
+++ app/src/main/java/com/pam/customlv/Movie.java
@@ -0,0 +1,30 @@
+package com.pam.customlv;
+
+public class Movie {
+    private String title;
+    private String year;
+
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setYear(String year){
+        this.year = year;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getYear(){
+        return year;
+    }
+
+
+}
